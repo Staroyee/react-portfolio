d8141403da9c05f30d38ee38e7babb873c02d1cd
Staroyee <danmasefield@gmail.com>
2023-10-24 18:27:22 +1030

    Installed Ant Design component library and experemented with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Aboutme from "./pages/Aboutme";
-import Portfolio from "./pages/Portfolio";
-import Contact from "./pages/Contact";
-import Resume from "./pages/Resume";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/Header";
+// import Footer from "./components/Footer";
+// import Aboutme from "./pages/Aboutme";
+// import Portfolio from "./pages/Portfolio";
+// import Contact from "./pages/Contact";
+// import Resume from "./pages/Resume";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="bg-warning min-vh-100 d-flex flex-column">
-        <Header />
-        <div className="flex-grow-1">
-          <Routes>
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="*" index element={<Aboutme />} />
-          </Routes>
-        </div>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <div className="bg-warning min-vh-100 d-flex flex-column">
+        <Nav />
+          
+    //     <Footer />
+    //   </div>
+    // </BrowserRouter>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,57 @@
-import { Link } from "react-router-dom";
+import { Layout, Menu } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+const { Header } = Layout;
 
-function Header() {
+function Nav() {
   return (
-    <>
-      <nav className="bg-secondary">
-        <ul>
-          <li>
-            <Link to="/">About Me</Link>
-          </li>
-          <li>
-            <Link to="/portfolio">Portfolio</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/resume">Resume</Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <Header
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <div className="demo-logo">
+          <img src="https://example.com/your-avatar-image.jpg" alt="Avatar" />
+        </div>
+        <h2 style={{ margin: 0, marginLeft: 16 }}>Daniel Masefield</h2>
+      </div>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+        <Menu.Item key="1">About Me</Menu.Item>
+        <Menu.Item key="2">Portfolio</Menu.Item>
+        <Menu.Item key="3">Resume</Menu.Item>
+        <Menu.Item key="4">Contact</Menu.Item>
+      </Menu>
+    </Header>
   );
 }
 
-export default Header;
+
+
+// import { Link } from "react-router-dom";
+
+// function Header() {
+//   return (
+//     <>
+//       <nav className="bg-secondary">
+//         <ul>
+//           <li>
+//             <Link to="/">About Me</Link>
+//           </li>
+//           <li>
+//             <Link to="/portfolio">Portfolio</Link>
+//           </li>
+//           <li>
+//             <Link to="/contact">Contact</Link>
+//           </li>
+//           <li>
+//             <Link to="/resume">Resume</Link>
+//           </li>
+//         </ul>
+//       </nav>
+//     </>
+//   );
+// }
+
+export default Nav;
